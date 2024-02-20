bfdff4cebfbfed6da296e44b0d7872f80cf92372
Oneplayer05 <aledipa512@gmail.com>
2024-02-20 12:03:22 +0100

    strat

---
+++ HTML/js/script.js
@@ -0,0 +1,3 @@
+alert("Benvenuto in questo sito")
+let diPasquale = window.prompt("Come ti chiami?")
+document.getElementById("di-pasquale").innerHTML = "Ciao " + diPasquale + ", benvenuto in questo sito"
