6df321dd16be42cddc6531b5e6c18ee86a844ac2
Oneplayer05 <aledipa512@gmail.com>
2024-03-12 11:21:57 +0100

    start

--- HTML/js/script.js
+++ HTML/js/script.js
@@ -1,27 +1,32 @@
-window.onload = function() {
-  var z, i, elmnt, file, xhttp;
-  /* Loop through a collection of all HTML elements: */
-  z = document.getElementsByTagName("*");
-  for (i = 0; i < z.length; i++) {
-    elmnt = z[i];
-    /*search for elements with a certain atrribute:*/
-    file = elmnt.getAttribute("w3-include-html");
-    if (file) {
-      /* Make an HTTP request using the attribute value as the file name: */
-      xhttp = new XMLHttpRequest();
-      xhttp.onreadystatechange = function() {
-        if (this.readyState == 4) {
-          if (this.status == 200) {elmnt.innerHTML = this.responseText;}
-          if (this.status == 404) {elmnt.innerHTML = "Page not found.";}
-          /* Remove the attribute, and call this function once more: */
-          elmnt.removeAttribute("w3-include-html");
-          includeHTML();
-        }
-      }
-      xhttp.open("GET", file, true);
-      xhttp.send();
-      /* Exit the function: */
-      return;
+function getPartialElements() {
+    var z, i, elmnt, elmnts;
+    elmnts = [];
+    z = document.getElementsByTagName("*");
+    for (i = 0; i < z.length; i++) {
+        elmnt = z[i];
+        elmnts.push(elmnt);
     }
-  }
+    return elmnts;
 };
+
+function createPartialElements(elementsList) {
+    elementsList.forEach(element => {
+        var file = element.getAttribute("partial-html");
+        if (file) {
+            var xhttp = new XMLHttpRequest();
+            xhttp.onreadystatechange = function() {
+                if (this.readyState == 4) {
+                    if (this.status == 200) {element.innerHTML = this.responseText;}
+                    if (this.status == 404) {element.innerHTML = "Page not found.";}
+                    element.removeAttribute("partial-html");
+                }
+            }
+            xhttp.open("GET", file, true);
+            xhttp.send();
+        }
+    });
+};
+
+window.onload = function() {
+    createPartialElements(getPartialElements());
+}
