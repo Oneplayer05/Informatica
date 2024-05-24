bf2420b904e9d0dbc9d9fb4313bf9710fa999ab3
Oneplayer05 <aledipa512@gmail.com>
2024-05-24 19:21:37 +0200

    c'è ancora da lavorare

--- HTML/js/script.js
+++ HTML/js/script.js
@@ -1,3 +1,4 @@
+
 function getPartialElements() {
     var z, i, elmnt, elmnts;
     elmnts = [];
@@ -35,3 +36,46 @@ function toggleSearch() {
     const searchContainer = document.querySelector('.search-container');
     searchContainer.classList.toggle('active');
 }
+const video = document.querySelector("video");
+const muteIcon = document.querySelector(".fa-volume-mute");
+const muteButtom = muteIcon.parentNode;
+muteButtom.addEventListener("click", () => {
+    if (video.muted == true){
+    video.muted = false;
+    muteIcon.classList.remove("fa-volume-mute");
+    muteIcon.classList.add("fa-volume-up");}
+    else {
+    video.muted = true;
+    muteIcon.classList.remove("fa-volume-up");
+    muteIcon.classList.add("fa-volume-mute");}
+ });
+ const images = document.querySelectorAll(".primodiv");
+ images.forEach ((image) => {
+    image.addEventListener("mouseenter",() => {
+    image.style.transform = "scale(1.05)"
+ });
+ image.addEventListener("mouseleave",() => {
+    image.style.transform = "scale(1)"
+ })});
+ const button = document.createElement("button");
+ document.body.appendChild(button);
+ button.innerHTML = ' <i class="fas fa-chevron-up"></i>';
+ button.style.position = "fixed";
+ button.style.bottom = "20px";
+ button.style.right = "20px";
+ button.style.display = "none";
+
+ button.addEventListener("click",() => {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
+ });
+ window.addEventListener("scroll",() => {
+    if(window.scrollY > 500){
+        button.style.display = "block";
+    }
+    else{
+        button.style.display = "none";
+    }
+ });
